6d84acb781ec64ff3817ea83d2b62979ea205b72
yasminsenior02 <yasminsenior02@gmail.com>
2022-07-07 16:30:45 -0700

    step 6 FINALLY done

--- vaccine-hub-api/models/user.js
+++ vaccine-hub-api/models/user.js
@@ -1,10 +1,68 @@
 const db = require("../db");
-const { UnauthorizedError } = require("../utils/errors");
+const { BadRequestError, UnauthorizedError } = require("../utils/errors");
 
 class User {
   static async login(credentials) {
     throw new UnauthorizedError("Invalid email/password combo");
   }
-  static async register(credentials) {}
+
+  static async register(credentials) {
+    const requiredField = [
+      "first_name",
+      "last_name",
+      "password",
+      "email",
+      "location",
+    ];
+    requiredField.forEach((field) => {
+      if (!credentials.hasOwnProperty(field)) {
+        throw new BadRequestError(`Missing ${field} in request body.`);
+      }
+    });
+
+    const existingUser = await User.fetchUserByEmail(credentials.email);
+
+    if (existingUser) {
+      throw new BadRequestError(`Duplicate email: ${credentials.email}`);
+    }
+
+    const lowercasedEmail = credentials.email.toLowerCase();
+    console.log(lowercasedEmail);
+
+    const result = await db.query(
+      `
+    INSERT INTO users (
+      first_name, last_name, password, email, location
+    )
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING id, first_name, last_name, password, email, location, date;
+    `,
+      [
+        credentials.first_name,
+        credentials.last_name,
+        credentials.password,
+        lowercasedEmail,
+        credentials.location,
+      ]
+    );
+
+    const user = result.rows[0];
+    console.log(user);
+    return user;
+  }
+  static async fetchUserByEmail(email) {
+    if (!email) {
+      throw new BadRequestError("No email provided");
+    }
+    console.log(email);
+    // const query = "SELECT * FROM users WHERE email = $1;";
+    // const result = await db.query(query, [email.toLowerCase()]);
+    const result = await db.query("SELECT * FROM users;");
+
+    console.log(2);
+    const user = result.rows[0];
+
+    return user;
+  }
 }
 module.exports = User;
--- vaccine-registration-ui/src/components/Login/Login.js
+++ vaccine-registration-ui/src/components/Login/Login.js
@@ -1,53 +1,59 @@
-import { useState } from "react"
-import { Link, useNavigate } from "react-router-dom"
-import axios from "axios"
-import MedicalResearch from "../MedicalResearch/MedicalResearch"
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import MedicalResearch from "../MedicalResearch/MedicalResearch";
 // import undraw_medical_research from "../../assets/undraw_medical_research_deep_blue.svg"
-import "./Login.css"
+import "./Login.css";
 
 export default function Login({ setAppState }) {
-  const navigate = useNavigate()
-  const [isLoading, setIsLoading] = useState(false)
-  const [errors, setErrors] = useState({})
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [errors, setErrors] = useState({});
   const [form, setForm] = useState({
     email: "",
     password: "",
-  })
+  });
 
   const handleOnInputChange = (event) => {
     if (event.target.name === "email") {
       if (event.target.value.indexOf("@") === -1) {
-        setErrors((e) => ({ ...e, email: "Please enter a valid email." }))
+        setErrors((e) => ({ ...e, email: "Please enter a valid email." }));
       } else {
-        setErrors((e) => ({ ...e, email: null }))
+        setErrors((e) => ({ ...e, email: null }));
       }
     }
 
-    setForm((f) => ({ ...f, [event.target.name]: event.target.value }))
-  }
+    setForm((f) => ({ ...f, [event.target.name]: event.target.value }));
+  };
 
   const handleOnSubmit = async (e) => {
-    e.preventDefault()
-    setIsLoading(true)
-    setErrors((e) => ({ ...e, form: null }))
+    e.preventDefault();
+    setIsLoading(true);
+    setErrors((e) => ({ ...e, form: null }));
 
     try {
-      const res = await axios.post(`http://localhost:3001/auth/login`, form)
+      const res = await axios.post(`http://localhost:3001/auth/login`, form);
       if (res?.data) {
-        setAppState(res.data)
-        setIsLoading(false)
-        navigate("/portal")
+        setAppState(res.data);
+        setIsLoading(false);
+        navigate("/portal");
       } else {
-        setErrors((e) => ({ ...e, form: "Invalid username/password combination" }))
-        setIsLoading(false)
+        setErrors((e) => ({
+          ...e,
+          form: "Invalid username/password combination",
+        }));
+        setIsLoading(false);
       }
     } catch (err) {
-      console.log(err)
-      const message = err?.response?.data?.error?.message
-      setErrors((e) => ({ ...e, form: message ? String(message) : String(err) }))
-      setIsLoading(false)
+      console.log(err);
+      const message = err?.response?.data?.error?.message;
+      setErrors((e) => ({
+        ...e,
+        form: message ? String(message) : String(err),
+      }));
+      setIsLoading(false);
     }
-  }
+  };
 
   return (
     <div className="Login">
@@ -84,7 +90,9 @@ export default function Login({ setAppState }) {
               value={form.password}
               onChange={handleOnInputChange}
             />
-            {errors.password && <span className="error">{errors.password}</span>}
+            {errors.password && (
+              <span className="error">{errors.password}</span>
+            )}
           </div>
 
           <button className="btn" disabled={isLoading} onClick={handleOnSubmit}>
@@ -99,5 +107,5 @@ export default function Login({ setAppState }) {
         </div>
       </div>
     </div>
-  )
+  );
 }
