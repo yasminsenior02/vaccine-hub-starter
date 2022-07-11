bd4c32b31726bdd0677af9eba105ec60e910ef8f
yasminsenior02 <yasminsenior02@gmail.com>
2022-07-10 23:12:26 -0700

    fully working

--- vaccine-hub-api/routes/auth.js
+++ vaccine-hub-api/routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const User = require("../models/user");
 const router = express.Router();
 
-router.post("login", async (Req, res, next) => {
+router.post("/login", async (req, res, next) => {
   try {
     const user = await User.login(req.body);
     return res.status(200).json({ user });
